486c96375b85576588854a9caed98e5a4ba4a131
Nami Tran <nami.tran@icloud.com>
2022-06-22 12:34:14 -0600

    books.js

--- public/src/books.js
+++ public/src/books.js
@@ -8,9 +8,23 @@ function findBookById(books, id) {
   return found
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let arr1 = books.filter((book) => book.borrows[0].returned === false);
+  let arr2 = books.filter((book) => book.borrows[0].returned !== false)
+  return [arr1,arr2 ]  
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let result = book.borrows.map(borrower => { 
+    let result = accounts.find(account => borrower.id === account.id )
+    result.returned = borrower.returned 
+    
+    return result
+   })
+   console.log(result)
+   return result.filter((borrower, i)=> result.findIndex(item => item.id === borrower.id) === i) 
+ 
+ }
 
 module.exports = {
   findAuthorById,
